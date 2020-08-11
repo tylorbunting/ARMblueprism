44461fc45b374d86421fde4e1c258f0ed23b68e3
Tylor Bunting <tb@tylorbunting.com>
2020-08-11 15:47:57 +1000

    connectazfiledrive.ps1 script add

---
+++ connectazfiledrive.ps1
@@ -0,0 +1,10 @@
+$connectTestResult = Test-NetConnection -ComputerName aihubautostorageaccount.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    $var_password=$args[0]
+    cmd.exe /C "cmdkey /add:`"aihubautostorageaccount.file.core.windows.net`" /user:`"Azure\aihubautostorageaccount`" /pass:`"$var_password`""
+    # Mount the drive
+    New-PSDrive -Name R -PSProvider FileSystem -Root "\\aihubautostorageaccount.file.core.windows.net\automationfileshare" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
