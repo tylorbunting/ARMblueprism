5adae45a84a470ff9d6737a84f940c74cd8d99f5
buntoss@gmail.com <buntoss@gmail.com>
2020-09-08 13:25:02 +1000

    commented out args

--- connectazfiledrive.ps1
+++ connectazfiledrive.ps1
@@ -1,7 +1,7 @@
 $connectTestResult = Test-NetConnection -ComputerName aihubautostorageaccount.file.core.windows.net -Port 445
 if ($connectTestResult.TcpTestSucceeded) {
     # Save the password so the drive will persist on reboot
-    $var_password=$args[0]
+    # $var_password=$args[0]
     cmd.exe /C "cmdkey /add:`"aihubautostorageaccount.file.core.windows.net`" /user:`"Azure\aihubautostorageaccount`" /pass:`"f1ufjeuly2ha8XtydNcooikU9wKOQC7aVo6+ODuUf+Mt6yORfih4XE6AYe0ZF/pvGei4+i+0m+/wjORhPZhhYw==`""
     # Mount the drive
     New-PSDrive -Name R -PSProvider FileSystem -Root "\\aihubautostorageaccount.file.core.windows.net\automationfileshare" -Persist
